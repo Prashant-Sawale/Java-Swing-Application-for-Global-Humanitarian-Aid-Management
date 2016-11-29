a79b2f5df55587922844288f2dd921bf4914eaf4
Prashant.Sawale <sawale.p@husky.neu.edu>
2016-11-29 13:19:20 -0500

    Classes and some are relationships established..

---
+++ Final_Project/src/business/Organization/MedicationOrganization.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.Organization;
+/**
+ *
+ * @author Admin
+ */
+public class MedicationOrganization extends  Organization{
+    
+}
